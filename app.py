24c66ff053fafcd1a64c843c6e5a761f3fa28790
sakshipatil <sakshipatil005@gmail.com>
2019-03-14 11:13:10 +0530

    I COMPLETED MY FIRST TASK

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
 
 # creates a Flask application, named app
@@ -8,17 +8,40 @@ app = Flask(__name__)
 @app.route("/login")
 def login():
     return render_template('Login-Page.html')
-    return render_template('css.css')
 
 @app.route("/register")
 def register():
     return render_template('Register-Page.html')
-    return render_template('style.css')
 
 @app.route("/dashboard")
 def dashboard():
     return render_template('Dashboard-Page.html')
-    return render_template('style1.css')
+
+@app.route("/login_submit",methods=["POST"])
+def login_submit():
+    username = request.form['username']
+    password = request.form['password']
+    print(username,password)
+    return render_template('Dashboard-Page.html',username=username)
+
+@app.route("/register_submit",methods=["POST"])
+def register_submit():
+    Firstname = request.form['fname']
+    Middlename = request.form['mname']
+    Lastname = request.form['lname']
+    username = request.form['username']
+    password = request.form['password']
+    rpassword = request.form['rpassword']
+    print(Firstname, Middlename, Lastname, username, password, rpassword)
+    return render_template('Dashboard-Page.html', username=username)
+
+
+
+@app.route("/dashboard_logout", methods=["POST"])
+def dashboard_logout():
+
+    return render_template('Login-Page.html')
+
 # run the application
 if __name__ == "__main__":
     app.run(host='127.0.0.1',port=8080,debug=True)
