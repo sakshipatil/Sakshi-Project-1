f9d5bb118955eb7175d9589839d6d4b73dbef4b1
sakshipatil <sakshipatil005@gmail.com>
2019-03-18 16:38:07 +0530

    Docker image

--- app.py
+++ app.py
@@ -1,13 +1,37 @@
 from flask import Flask, request
 from flask import render_template
+from redis import Redis, RedisError
+import os
+import socket
+import pymongo
+
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+
+mydb = myclient["project1"]
+
+mycol = mydb["student"]
 
 # creates a Flask application, named app
 app = Flask(__name__)
 
+# connect a redis
+redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
+
 # a route where we will display a welcome message via an HTML templates
 @app.route("/login")
 def login():
-    return render_template('Login-Page.html')
+    try:
+        visits = redis.incr("counter")
+        return render_template('Login-Page.html')
+    except RedisError:
+        visits = "<i>cannot connect to Redis, counter disabled</i>"
+
+    html = "<h3>login {name}!</h3>" \
+           "<b>Hostname:</b> {hostname}<br/>" \
+           "<b>Visits:</b> {visits}"
+    return html.format(name=os.getenv("USERNAME", "project"), hostname=socket.gethostname(), visits=visits)
+
 
 @app.route("/register")
 def register():
@@ -22,18 +46,30 @@ def login_submit():
     username = request.form['username']
     password = request.form['password']
     print(username,password)
-    return render_template('Dashboard-Page.html',username=username)
+    login_sumbit = mycol.find_one({'name': username,'password':password})
+    if login_sumbit:
+        return render_template('Dashboard-Page.html',username=username)
+    return "Invalid Username"
 
 @app.route("/register_submit",methods=["POST"])
 def register_submit():
-    Firstname = request.form['fname']
-    Middlename = request.form['mname']
-    Lastname = request.form['lname']
-    username = request.form['username']
-    password = request.form['password']
-    rpassword = request.form['rpassword']
-    print(Firstname, Middlename, Lastname, username, password, rpassword)
-    return render_template('Dashboard-Page.html', username=username)
+    if request.method == 'POST':
+        Firstname = request.form['fname']
+        Middlename = request.form['mname']
+        Lastname = request.form['lname']
+        username = request.form['username']
+        password = request.form['password']
+        rpassword = request.form['rpassword']
+        print(Firstname, Middlename, Lastname, username, password, rpassword)
+
+        existing_user = mycol.find_one({'name' : username})
+
+        if existing_user is None:
+            mycol.insert({'fname':Firstname,'mname': Middlename,'lname':Lastname,'name' : username,'password' : password,'rpassword':rpassword})
+            return render_template('Dashboard-Page.html', username=username)
+        return 'That user Already Exists'
+    return render_template('register.html')
+
 
 
 
